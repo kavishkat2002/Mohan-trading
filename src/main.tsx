db562c1d56d6fd7eb6bcb5baa36e6e61c664b11b
kavishkat2002 <kavishkathilakarathna0@gmail.com>
2026-07-06 10:47:59 +0530

    fix: intercept all ngrok API requests to bypass warning page

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,34 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Global fetch override to bypass the ngrok warning page for API calls
+const originalFetch = window.fetch;
+window.fetch = function (input: RequestInfo | URL, init?: RequestInit) {
+  let url = '';
+  if (typeof input === 'string') {
+    url = input;
+  } else if (input instanceof URL) {
+    url = input.toString();
+  } else if (input instanceof Request) {
+    url = input.url;
+  }
+
+  if (url.includes('ngrok-free.dev')) {
+    if (input instanceof Request) {
+      input.headers.set('ngrok-skip-browser-warning', 'true');
+    } else {
+      init = init || {};
+      init.headers = init.headers || {};
+      if (init.headers instanceof Headers) {
+        init.headers.set('ngrok-skip-browser-warning', 'true');
+      } else if (Array.isArray(init.headers)) {
+        init.headers.push(['ngrok-skip-browser-warning', 'true']);
+      } else {
+        (init.headers as any)['ngrok-skip-browser-warning'] = 'true';
+      }
+    }
+  }
+  return originalFetch(input, init);
+};
+
 createRoot(document.getElementById("root")!).render(<App />);
